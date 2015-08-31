0a4569cb7d081c1d57723dc1b939fb8ef56a6823
Artem Kostiuk <postatum@gmail.com>
2015-08-31 16:19:12 +0300

    Initial project setup

---
+++ setup.py
@@ -0,0 +1,35 @@
+import os
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.md')).read()
+VERSION = open(os.path.join(here, 'VERSION')).read()
+
+requires = [
+
+]
+
+setup(name='nefertari-guards',
+      version=VERSION,
+      description='',
+      long_description=README,
+      classifiers=[
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 2",
+          "Programming Language :: Python :: 2.7",
+          "Programming Language :: Python :: 3",
+          "Programming Language :: Python :: 3.4",
+          "Framework :: Pyramid",
+          "Topic :: Internet :: WWW/HTTP",
+          "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+      ],
+      author='Brandicted',
+      author_email='hello@brandicted.com',
+      url='https://github.com/brandicted/nefertari-guards',
+      keywords='web pyramid pylons',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=requires,
+      tests_require=requires,
+      test_suite="nefertari_guards")
