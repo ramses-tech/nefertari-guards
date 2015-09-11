79d7a9a5fa0104dc105af8e881502e082ce6262c
Artem Kostiuk <postatum@gmail.com>
2015-09-11 12:58:46 +0300

    Add tests for DocumentACLMixin

--- tests/test_documents.py
+++ tests/test_documents.py
@@ -1,84 +1,57 @@
-from mock import patch, call
-from nefertari_guards import elasticsearch as es
+from mock import Mock
 
-from .fixtures import engine_mock
-
-
-class TestESHelpers(object):
-
-    def test_build_acl_bool_terms(self, engine_mock):
-        from pyramid.security import Allow
-        engine_mock.ACLField.stringify_acl.return_value = [
-            {'principal': 'user', 'permission': 'view'},
-            {'principal': 'admin', 'permission': 'create'},
-            {'principal': 'admin', 'permission': 'view'},
-        ]
-        engine_mock.ACLField._stringify_action.return_value = 'allow'
-        terms = es._build_acl_bool_terms('zoo', Allow)
-        engine_mock.ACLField.stringify_acl.assert_called_once_with('zoo')
-        engine_mock.ACLField._stringify_action.assert_called_once_with(Allow)
-        assert len(terms) == 3
-        assert {'term': {'_acl.action': 'allow'}} in terms
-        assert {'terms': {'_acl.principal': ['admin', 'user']}} in terms
-        assert {'terms': {'_acl.permission': ['create', 'view']}} in terms
-
-    def test_build_acl_from_principals(self):
-        from pyramid.security import Deny, ALL_PERMISSIONS
-        acl = es._build_acl_from_principals(['admin', 'user'], Deny)
-        assert (Deny, 'user', 'view') in acl
-        assert (Deny, 'user', ALL_PERMISSIONS) in acl
-        assert (Deny, 'admin', 'view') in acl
-        assert (Deny, 'admin', ALL_PERMISSIONS) in acl
-
-    @patch('nefertari_guards.elasticsearch._build_acl_bool_terms')
-    @patch('nefertari_guards.elasticsearch._build_acl_from_principals')
-    def test_build_acl_query(self, build_ids, build_terms):
-        from pyramid.security import Deny, Allow
-        build_ids.return_value = [(1, 2, 3)]
-        build_terms.return_value = 'foo'
-        query = es.build_acl_query(['user', 'admin'])
-        build_ids.assert_has_calls([
-            call(['user', 'admin'], Allow),
-            call(['user', 'admin'], Deny),
-        ])
-        build_terms.assert_has_calls([
-            call([(1, 2, 3)], Allow),
-            call([(1, 2, 3)], Deny),
-        ])
-        must = must_not = {
-            'nested': {
-                'path': '_acl',
-                'filter': {'bool': {'must': 'foo'}}
-            }
-        }
-        assert query == {
-            'filter': {
-                'bool': {
-                    'must': must,
-                    'must_not': must_not
-                }
-            }
-        }
+from nefertari_guards import documents as docs
 
 
-class TestACLFilterES(object):
+class TestDocumentACLMixin(object):
 
-    @patch('nefertari_guards.elasticsearch.build_acl_query')
-    def test_build_search_params(self, mock_build):
-        obj = es.ACLFilterES('Foo', 'foondex', chunk_size=10)
-        mock_build.return_value = {'filter': 'zoo'}
-        params = obj.build_search_params(
-            {'foo': 1, '_limit': 10, '_principals': [3, 4]})
-        assert sorted(params.keys()) == sorted([
-            'body', 'doc_type', 'from_', 'size', 'index'])
-        assert params['body'] == {
-            'query': {
-                'filtered': {
-                    'filter': 'zoo',
-                    'query': {'query_string': {'query': 'foo:1'}}
-                }
-            }
-        }
-        assert params['index'] == 'foondex'
-        assert params['doc_type'] == 'Foo'
-        mock_build.assert_called_once_with([3, 4])
+    def _mocked_document_cls(self):
+        engine = Mock()
+        document_cls = docs.get_document_mixin(engine)
+        document_cls._engine_mock = engine
+        document_cls.pk_field = Mock(return_value='id')
+        document_cls.id = 1
+        return document_cls
+
+    def test_default_item_acl(self):
+        document_cls = self._mocked_document_cls()
+        document_cls.__item_acl__ = 'foo'
+        assert document_cls.default_item_acl() == 'foo'
+
+    def test_get_acl(self):
+        document_cls = self._mocked_document_cls()
+        document = document_cls()
+        document._acl = 'foo'
+        result = document.get_acl()
+        field = document_cls._engine_mock.ACLField
+        field.objectify_acl.assert_called_once_with('foo')
+        assert result == field.objectify_acl()
+
+    def test_set_default_acl(self):
+        document_cls = self._mocked_document_cls()
+        document_cls.__item_acl__ = 'foo'
+        document = document_cls()
+        document._is_created = Mock(return_value=True)
+        document._acl = None
+        document._set_default_acl()
+        field = document_cls._engine_mock.ACLField
+        field.stringify_acl.assert_called_once_with('foo')
+        assert document._acl == field.stringify_acl()
+
+    def test_set_default_acl_not_created(self):
+        document_cls = self._mocked_document_cls()
+        document_cls.__item_acl__ = 'foo'
+        document = document_cls()
+        document._is_created = Mock(return_value=False)
+        document._acl = None
+        document._set_default_acl()
+        assert document._acl is None
+
+    def test_set_default_acl_already_set(self):
+        document_cls = self._mocked_document_cls()
+        document_cls.__item_acl__ = 'foo'
+        document = document_cls()
+        document._is_created = Mock(return_value=True)
+        document._acl = 123
+        document._set_default_acl()
+        assert document._acl == 123
