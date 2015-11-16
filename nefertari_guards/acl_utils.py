a49d0481646f3b3e4e926137edf100347e8b950c
Artem Kostiuk <postatum@gmail.com>
2015-11-16 14:24:53 +0200

    Implement base to query by ACE

--- nefertari_guards/acl_utils.py
+++ nefertari_guards/acl_utils.py
@@ -1,3 +1,22 @@
+"""
+CLI to manage db-ACLs:
+
+$ nefertari-guards.count_ace
+  --ace='{"action":"<action>","permission:"<permission>","principal":"<principal>"}'
+-> returns count of objects with matching ACE, listed by type
+
+$ nefertari-guards.update_ace
+  --from_ace='{"action":"<action>","permission:"<permission>","principal":"<principal>"}'
+  --to_ace='{"action":"<action>","permission:"<permission>","principal":"<principal>"}'
+  --types=<list_of_document_types>
+-> updates all objects one ACE at a time, option to restrict type(s)
+
+API will be almost identical, e.g. nefertari_guards.count_ace(ace={...})
+"""
+from nefertari import engine
+from nefertari.elasticsearch import ES
+
+
 def count_ace():
     pass
 
@@ -6,5 +25,45 @@ def update_ace():
     pass
 
 
-def find_containing_ace():
-    pass
+def find_containing_ace(ace, types=None, count=False):
+    if types is None:
+        types = list(engine.get_document_classes().values())
+    es_types = _get_es_types(types)
+
+    params = {'body': _get_es_body(ace)}
+    if count:
+        params['_count'] = True
+
+    return ES(es_types).get_collection(**params)
+
+
+def _get_es_types(types):
+    type_names = [t.__name__ for t in types
+                  if getattr(t, '_index_enabled', False)]
+    es_types = [ES.src2type(name) for name in type_names]
+    return ','.join(es_types)
+
+
+def _get_es_body(ace):
+    must = [
+        {'term': {'_acl.action': ace['action']}},
+        {'term': {'_acl.principal': ace['principal']}},
+        {'term': {'_acl.permission': ace['permission']}}
+    ]
+    body = {
+        'query': {
+            'filtered': {
+                'filter': {
+                    'nested': {
+                        'filter': {
+                            'bool': {
+                                'must': must
+                            }
+                        },
+                        'path': '_acl'
+                    }
+                }
+            }
+        }
+    }
+    return {'body': body}
