8e0bbfe505cc8db7326338fccd59d31720633692
Artem Kostiuk <postatum@gmail.com>
2015-11-16 11:48:43 +0200

    Add base for ACE api

--- nefertari_guards/__init__.py
+++ nefertari_guards/__init__.py
@@ -1,3 +1,10 @@
+from .acl_utils import (
+    count_ace,
+    update_ace,
+    find_containing_ace,
+)
+
+
 def includeme(config):
     config.include('nefertari_guards.engine')
     config.include('nefertari_guards.elasticsearch')
---
+++ nefertari_guards/acl_utils.py
@@ -0,0 +1,10 @@
+def count_ace():
+    pass
+
+
+def update_ace():
+    pass
+
+
+def find_containing_ace():
+    pass
